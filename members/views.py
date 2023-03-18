a149195221b5c0440ab2f07da55578e070e2ff4e
Sebika Nepal <rogina.nep47@gmail.com>
2023-03-18 21:52:10 +0545

    first commit

---
+++ members/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.template import loader
+# Create your views here.
+from django.http import HttpResponse
+
+def members(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
